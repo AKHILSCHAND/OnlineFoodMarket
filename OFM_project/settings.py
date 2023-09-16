c8d57fc1b6716b3118c8c4f43eb46de23fbb197b
akhilschand <nandhuakhil95@gmail.com>
2023-09-16 19:41:33 +0530

    media files configuration

--- OFM_project/settings.py
+++ OFM_project/settings.py
@@ -130,6 +130,13 @@ STATICFILES_DIRS = [
     'OFM_project/static'
 ]
 
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
