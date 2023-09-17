2719c62c54c7f24a198f4cb0e88d437ce5ea2c46
akhilschand <nandhuakhil95@gmail.com>
2023-09-17 15:52:05 +0530

    created vendor model, templates, vendoradmin confg, and registration features

--- vendor_app/admin.py
+++ vendor_app/admin.py
@@ -1,6 +1,13 @@
+
 from django.contrib import admin
 from vendor_app.models import Vendor
 
 # Register your models here.
 
-admin.site.register(Vendor)
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+    list_editable = ('is_approved',)
+
+admin.site.register(Vendor,VendorAdmin)
