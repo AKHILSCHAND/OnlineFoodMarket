650fbe176de1ed9b8f3b6f1c634c8b6a916bda5e
akhilschand <nandhuakhil95@gmail.com>
2023-09-20 12:36:06 +0530

    vendor profile form and validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import User
+from .models import User,UserProfile
+from .validators import allow_only_images_validator
 
 
 
@@ -23,3 +24,29 @@ class UserForm(forms.ModelForm):
             raise forms.ValidationError(      # this validation error is called non field errors
                 "Password does not match!"
             )
+            
+
+class UserProfileForm(forms.ModelForm):
+
+    # profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    # cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    # if we are using custom validator, change the ImageField to FileField otherwise it wont work
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),validators=[allow_only_images_validator])
+    
+    # making read only form for latitude and longitude
+    
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+        
+    # another way of making read only form for latitude and longitude    
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
